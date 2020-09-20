dbbc94eaefedcef8f9b6256a70efb78c1b26cd4f
Denniel William <dennielwilliam.unb@gmail.com>
2020-09-20 08:16:24 -0300

    Estilizaçao da pagina Materias

--- src/paginas/Materias/index.js
+++ src/paginas/Materias/index.js
@@ -1,83 +1,93 @@
 import React from 'react';
 import {View, Image, Text, TouchableOpacity} from 'react-native';
 import { Header } from 'react-native/Libraries/NewAppScreen';
-import { Container, BotaoMateria, TextoMateria, BlocoMaterias, BotaoContinuar, TextoContinuar, ImagemMateria } from './styles';
+import { BotaoMateria, TextoMateria, BlocoMaterias, BotaoContinuar, TextoContinuar } from './styles';
+import { BlueContainer, WhiteContainer, ImagemMateria, ContainerMaterias, ContainerUser } from './styles';
 
 export default function Materias(){
   return(
-    <Container>
-      <BlocoMaterias>
-        <BotaoMateria>
-          <ImagemMateria source={require("../../assets/icons/fisica.png")} />
-          <TextoMateria> Português </TextoMateria>
-        </BotaoMateria>
+    <>
+    <BlueContainer>
+      <ContainerUser>
+        <ImagemMateria source={require("../../assets/icons/user.png")} />
+      </ContainerUser>
+    </BlueContainer>
+    <WhiteContainer>
+    </WhiteContainer>
+    <ContainerMaterias>
+    <BlocoMaterias>
+      <BotaoMateria>
+        <ImagemMateria source={require("../../assets/icons/portugues.png")} />
+        <TextoMateria> Português </TextoMateria>
+      </BotaoMateria>
 
-        <BotaoMateria>
-          <ImagemMateria source={require("../../assets/icons/fisica.png")} />
-          <TextoMateria> Matemática </TextoMateria>
-        </BotaoMateria>
+      <BotaoMateria>
+        <ImagemMateria source={require("../../assets/icons/matematica.png")} />
+        <TextoMateria> Matemática </TextoMateria>
+      </BotaoMateria>
 
-        <BotaoMateria>
-          <ImagemMateria source={require("../../assets/icons/fisica.png")} />
-          <TextoMateria> História </TextoMateria>
-        </BotaoMateria>
-      </BlocoMaterias>
-      
-      <BlocoMaterias>
-        <BotaoMateria>
-          <ImagemMateria source={require("../../assets/icons/fisica.png")} />
-          <TextoMateria> Geografia </TextoMateria>
-        </BotaoMateria>
+      <BotaoMateria>
+        <ImagemMateria source={require("../../assets/icons/historia.png")} />
+        <TextoMateria> História </TextoMateria>
+      </BotaoMateria>
+    </BlocoMaterias>
+    
+    <BlocoMaterias>
+      <BotaoMateria>
+        <ImagemMateria source={require("../../assets/icons/geografia.png")} />
+        <TextoMateria> Geografia </TextoMateria>
+      </BotaoMateria>
 
-        <BotaoMateria>
-          <ImagemMateria source={require("../../assets/icons/fisica.png")} />
-          <TextoMateria> Ciência </TextoMateria>
-        </BotaoMateria>
+      <BotaoMateria>
+        <ImagemMateria source={require("../../assets/icons/ciencia.png")} />
+        <TextoMateria> Ciência </TextoMateria>
+      </BotaoMateria>
 
-        <BotaoMateria>
-          <ImagemMateria source={require("../../assets/icons/fisica.png")} />
-          <TextoMateria> Química </TextoMateria>
-        </BotaoMateria>
-      </BlocoMaterias>
+      <BotaoMateria>
+        <ImagemMateria source={require("../../assets/icons/quimica.png")} />
+        <TextoMateria> Química </TextoMateria>
+      </BotaoMateria>
+    </BlocoMaterias>
 
-      <BlocoMaterias>
-        <BotaoMateria>
-          <ImagemMateria source={require("../../assets/icons/fisica.png")} />
-          <TextoMateria> Física </TextoMateria>
-        </BotaoMateria>
+    <BlocoMaterias>
+      <BotaoMateria>
+        <ImagemMateria source={require("../../assets/icons/fisica.png")} />
+        <TextoMateria> Física </TextoMateria>
+      </BotaoMateria>
 
-        <BotaoMateria>
-          <ImagemMateria source={require("../../assets/icons/fisica.png")} />
-          <TextoMateria> Biologia </TextoMateria>
-        </BotaoMateria>
+      <BotaoMateria>
+        <ImagemMateria source={require("../../assets/icons/biologia.png")} />
+        <TextoMateria> Biologia </TextoMateria>
+      </BotaoMateria>
 
-        <BotaoMateria>
-          <ImagemMateria source={require("../../assets/icons/fisica.png")} />
-          <TextoMateria> Redação </TextoMateria>
-        </BotaoMateria>
-      </BlocoMaterias>
+      <BotaoMateria>
+        <ImagemMateria source={require("../../assets/icons/redacao.png")} />
+        <TextoMateria> Redação </TextoMateria>
+      </BotaoMateria>
+    </BlocoMaterias>
 
-      <BlocoMaterias>
-        <BotaoMateria>
-          <ImagemMateria source={require("../../assets/icons/fisica.png")} />
-          <TextoMateria> Artes </TextoMateria>
-        </BotaoMateria>
+    <BlocoMaterias>
+      <BotaoMateria>
+        <ImagemMateria source={require("../../assets/icons/artes.png")} />
+        <TextoMateria> Artes </TextoMateria>
+      </BotaoMateria>
 
-        <BotaoMateria>
-          <ImagemMateria source={require("../../assets/icons/fisica.png")} />
-          <TextoMateria> Inglês </TextoMateria>
-        </BotaoMateria>
+      <BotaoMateria>
+        <ImagemMateria source={require("../../assets/icons/ingles.png")} />
+        <TextoMateria> Inglês </TextoMateria>
+      </BotaoMateria>
 
-        <BotaoMateria>
-          <ImagemMateria source={require("../../assets/icons/fisica.png")} />
-          <TextoMateria> Espanhol </TextoMateria>
-        </BotaoMateria>
-      </BlocoMaterias>
+      <BotaoMateria>
+        <ImagemMateria source={require("../../assets/icons/espanhol.png")} />
+        <TextoMateria> Espanhol </TextoMateria>
+      </BotaoMateria>
+    </BlocoMaterias>
 
-      <BotaoContinuar>
-        <TextoContinuar> Continuar </TextoContinuar>
-      </BotaoContinuar>
-    </Container>
+    <BotaoContinuar>
+      <TextoContinuar> Continuar </TextoContinuar>
+    </BotaoContinuar>
+  </ContainerMaterias>
+  </>
   );
 };
 
