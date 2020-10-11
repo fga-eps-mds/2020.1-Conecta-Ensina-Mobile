a9d2f90b4199021f467dc6de1b924203396abf05
Edvan <edvan.bgjunior@gmail.com>
2020-10-11 11:20:19 -0300

    Creating AulaPendente page
    Co-authored-by: Denniel William <dennielwilliam.unb@gmail.com>

--- src/pages/AulaPendente/index.js
+++ src/pages/AulaPendente/index.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Theme, {theme} from '../../../Theme';
 import Background1 from '../../components/Background1';
+import {ContainerVisualAula, ListaVisualAula,
+        ContainerButtons, ContainerTexto,
+        ButtonConfirmar, ButtonRecusar} from './styles'
+import {Text} from 'react-native'
+
+const Item = ({item, onPress, style}) => (
+    <ContainerVisualAula/>
+  );
 
 export default function AulaPendente(){
+
+    const [filtros,setFiltros] = useState([
+        {id: '1', nome: 'Reforço Escolar', img: require('../../assets/books.png')},
+        {id: '2', nome: 'Idiomas', img: require('../../assets/books.png')},
+        {id: '3', nome: 'Vestibular', img: require('../../assets/books.png')},
+      ]);
+
+    const renderItem = ({item}) => {
+        
+        return (
+            <ContainerVisualAula>
+                <ContainerTexto>
+                </ContainerTexto>
+                <ContainerButtons>
+                    <ButtonRecusar>
+                    </ButtonRecusar>
+                    <ButtonConfirmar>
+                    </ButtonConfirmar>
+                </ContainerButtons>
+            </ContainerVisualAula>
+        );
+    };
 
     return(
         <Theme>
             <Background1>
+                <ListaVisualAula
+                    numColumns={2}
+                    data={filtros}
+                    keyExtractor={(item)=>item.key}
+                    renderItem={renderItem}
+                />
             </Background1>
         </Theme>
     );
---
+++ src/pages/AulaPendente/styles.js
@@ -0,0 +1,39 @@
+import styled from  'styled-components'
+
+export const ContainerVisualAula = styled.View`
+    background-color: ${(props) => props.theme.colors.cinzaClaro};
+    width: 144px;
+    height: 223px;
+    border-radius: 10px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+`; 
+
+export const ListaVisualAula = styled.FlatList`
+    left: 10%;
+    position: absolute;
+    padding-bottom: 25%;
+    align-self: center; 
+`;
+
+export const ButtonConfirmar = styled.TouchableOpacity` 
+    width: 35px;
+    height: 35px    
+    background-color: green;
+`;
+
+export const ButtonRecusar = styled.TouchableOpacity`
+    width: 35px;
+    height:35px;
+    background-color: red;
+`;
+
+export const ContainerButtons = styled.View`
+    justify-content: flex-end;
+    height: 35px;
+    width: 95px;
+`;
+
+export const ContainerTexto = styled.View`
+    
+`;
