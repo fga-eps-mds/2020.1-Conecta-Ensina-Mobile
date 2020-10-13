d5804ee516968cbe35d134323d3a89e5828c251f
IgorVeludo <velvet.guimaraes@gmail.com>
2020-10-12 22:37:17 -0300

    Fix styles.js AulaRealizada for test

--- src/pages/AulaRealizada/styles.js
+++ src/pages/AulaRealizada/styles.js
@@ -1,4 +1,4 @@
-import styled from  'styled-components'
+import styled from  'styled-components/native'
 
 export const ContainerVisualAula = styled.View`
     background-color: ${(props) => props.theme.colors.cinzaClaro};
